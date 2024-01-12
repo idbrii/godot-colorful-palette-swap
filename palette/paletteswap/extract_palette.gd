155209a3f7a7468465bf96f4167528c483f60037
David Briscoe <idbrii@gmail.com>
2024-01-12 09:53:34 -0800

    Support palette generation by greys or hues
    For generating palettes from non pixel art images.

--- palette/paletteswap/extract_palette.gd
+++ palette/paletteswap/extract_palette.gd
@@ -17,25 +17,65 @@ class ColorSort:
 		# Sort by grey because I think that matches what paletter is expecting.
 		return grey(a) < grey(b)
 
+static func find_largest_saturation(color_list):
+	var max_color = color_list[0]
+	for c in color_list:
+		if c.s > max_color.s:
+			max_color = c
+	return max_color
 
 func _ready():
-	var image = Image.new()
-	image.load(target_image)
+	var image := Image.new()
+	Validate.ok(image.load(target_image))
 	image.lock()
 
 	var size = image.get_size()
 
-	var colors = {}
-
+	var colors := {}
+	var hues := {}
 	for x in size.x:
 		for y in size.y:
 			var c = image.get_pixel(x, y)
 			colors[c] = c
+			hues[c.h] = hues.get(c.h, [])
+			hues[c.h].append(c)
 
-	var color_list = []
-	for k in colors:
-		var c = colors[k]
-		color_list.append(c)
+	var color_list := []
+
+	# TODO: make an exported dropdown for this.
+	var filter_by_grey = true
+
+	if hues.size() < 200:
+		# Include all colours (pixel art).
+		for k in colors:
+			var c = colors[k]
+			color_list.append(c)
+
+	elif filter_by_grey:
+		var greys := []
+		for k in colors:
+			var c = colors[k]
+			greys.append(c)
+
+		greys.sort_custom(ColorSort, "sort_ascending")
+		var last_grey := -1.0
+		for c in greys:
+			var g = ColorSort.grey(c)
+			var delta = g - last_grey
+			if delta > 0.1:
+				color_list.append(c)
+				last_grey = g
+
+	else:
+		# Limit hues
+		var last_hue := -1.0
+		for h in hues:
+			var delta = h - last_hue
+			if delta > 0.005:
+				var c = find_largest_saturation(hues[h])
+				color_list.append(c)
+				last_hue = h
+
 
 	color_list.sort_custom(ColorSort, "sort_ascending")
 
@@ -46,12 +86,18 @@ func _ready():
 
 func create_palette(file: String, colors: Array):
 	var out := Image.new()
-	out.create(colors.size(), 1, false, Image.FORMAT_RGB8)
+	var box_width := 1
+	var size_x := colors.size() * box_width
+	var size_y := box_width
+	out.create(size_x, size_y, false, Image.FORMAT_RGB8)
 
 	out.lock()
 	var i := 0
 	for c in colors:
-		out.set_pixel(i, 0, c)
+		var box_offset := box_width * i
+		for x in range(box_width):
+			for y in range(size_y):
+				out.set_pixel(box_offset + x, y, c)
 		i += 1
 	out.unlock()
 
