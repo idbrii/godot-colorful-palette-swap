b00a53f2e9f6557153eea70cdeac92f1f5cac18d
David Briscoe <idbrii@gmail.com>
2024-01-27 10:19:22 -0800

    Add file picker to ExtractPalette popup
    Convert path info into a scene and create a parent script to manage it
    and pop open the editor file picker.

---
+++ addons/colorful-palette-swap/panel/editor_path_input.gd
@@ -0,0 +1,48 @@
+tool
+extends Control
+
+const PathEdit = preload("res://addons/colorful-palette-swap/code/util/pathedit.gd")
+
+export var label := "Path"
+export var path_tooltip := "A file path."
+export(PathEdit.PathType) var path_type := PathEdit.PathType.EXISTING_FILE
+
+
+func _ready():
+	$Label.text = label
+	$Value.path_type = path_type
+	$Label.hint_tooltip = path_tooltip
+	$Value.hint_tooltip = path_tooltip
+	$PickFileButton.connect("pressed", self, "_on_pick_file_button")
+
+
+func _on_pick_file_button():
+	var popup := EditorFileDialog.new()
+	popup.disable_overwrite_warning = false
+	popup.current_file = $Value.get_path()
+
+	match $Value.path_type:
+		PathEdit.PathType.EXISTING_DIRECTORY, PathEdit.PathType.NEW_DIRECTORY:
+			popup.mode = EditorFileDialog.MODE_OPEN_DIR
+			popup.display_mode = EditorFileDialog.DISPLAY_LIST
+		PathEdit.PathType.EXISTING_FILE:
+			popup.mode = EditorFileDialog.MODE_OPEN_FILE
+			popup.display_mode = EditorFileDialog.DISPLAY_THUMBNAILS
+		PathEdit.PathType.NEW_FILE:
+			popup.mode = EditorFileDialog.MODE_SAVE_FILE
+			popup.display_mode = EditorFileDialog.DISPLAY_THUMBNAILS
+
+	add_child(popup)
+	popup.connect("dir_selected", self, "_on_path_selected")
+	popup.connect("file_selected", self, "_on_path_selected")
+	popup.popup_centered_minsize(Vector2(1500, 900))
+
+
+func _on_path_selected(path):
+	# Note: popup.current_* aren't valid to fetch and sometimes contain
+	# multiple concatenated paths.
+	$Value.set_text_and_validate(path)
+
+
+func get_path():
+	return $Value.get_path()
