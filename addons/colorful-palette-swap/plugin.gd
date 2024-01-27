c07cd965d3399929ba7d96e61b851377c25ed8dd
David Briscoe <idbrii@gmail.com>
2024-01-27 08:01:31 -0800

    Whitespace: Convert to tabs
    I hate them but the rest of the code is using tabs.

--- addons/colorful-palette-swap/plugin.gd
+++ addons/colorful-palette-swap/plugin.gd
@@ -8,14 +8,14 @@ var bar_btn: ToolButton
 
 
 func _enter_tree():
-    bar = bar_scene.instance()
-    bar.plugin = self
-    bar_btn = add_control_to_bottom_panel(bar, "Colorful Swap")
-    # Need extra space to push above toolbar buttons. Bottom margin didn't work.
-    bar.rect_min_size.y = bar.rect_size.y + 50
+	bar = bar_scene.instance()
+	bar.plugin = self
+	bar_btn = add_control_to_bottom_panel(bar, "Colorful Swap")
+	# Need extra space to push above toolbar buttons. Bottom margin didn't work.
+	bar.rect_min_size.y = bar.rect_size.y + 50
 
 
 func _exit_tree():
-    if bar:
-        remove_control_from_bottom_panel(bar)
-        bar.queue_free()
+	if bar:
+		remove_control_from_bottom_panel(bar)
+		bar.queue_free()
