ac72d581ed85f8a8bab4da0a3f91456e8da69ab0
David Briscoe <idbrii@gmail.com>
2024-01-27 07:57:51 -0800

    Add SwappablePalette as a popup dialog
    Instead of always-present bottom bar, make a popup from the Project >
    Tools menu.

--- addons/colorful-palette-swap/plugin.gd
+++ addons/colorful-palette-swap/plugin.gd
@@ -2,9 +2,11 @@ tool
 extends EditorPlugin
 
 const ExtractPalettePopup = preload("res://addons/colorful-palette-swap/panel/popup_extract_palette.tscn")
+const SwappablePalettePopup = preload("res://addons/colorful-palette-swap/panel/popup_swappable_palette.tscn")
 const bar_scene = preload("res://addons/colorful-palette-swap/panel/palette_commands.tscn")
 
 const MENUITEM_EXTRACT_PALETTE := "Extract Palette"
+const MENUITEM_SWAPPABLE_PALETTE := "Create Swap Palettes"
 
 var bar
 var bar_btn: ToolButton
@@ -18,6 +20,7 @@ func _enter_tree():
 	bar.rect_min_size.y = bar.rect_size.y + 50
 
 	add_tool_menu_item(MENUITEM_EXTRACT_PALETTE, self, "_extract_palette")
+	add_tool_menu_item(MENUITEM_SWAPPABLE_PALETTE, self, "_swappable_palette")
 
 
 func _show_popup(popup: WindowDialog, title: String) -> WindowDialog:
@@ -40,8 +43,22 @@ func _extract_palette(__):
 	get_editor_interface().get_resource_filesystem().scan()
 
 
+
+func _swappable_palette(__):
+	var source_image_path := get_editor_interface().get_current_path()
+	var input_path := "res://colorfulpalette-input/"
+	var output_path := "res://colorfulpalette-output/"
+	var popup := _show_popup(SwappablePalettePopup.instance(), MENUITEM_SWAPPABLE_PALETTE)
+	var swappable_palette = popup.get_node("%GenerateSwapPalettes")
+	swappable_palette.primary_palette_path_node.set_text_and_validate(source_image_path)
+	swappable_palette.input_path_node.set_text_and_validate(input_path)
+	swappable_palette.output_path_node.set_text_and_validate(output_path)
+	get_editor_interface().get_resource_filesystem().scan()
+
+
 func _exit_tree():
 	if bar:
 		remove_control_from_bottom_panel(bar)
 		bar.queue_free()
 	remove_tool_menu_item(MENUITEM_EXTRACT_PALETTE)
+	remove_tool_menu_item(MENUITEM_SWAPPABLE_PALETTE)
